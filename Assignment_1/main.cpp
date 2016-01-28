de8c72e9d5c60507e0dba488f7859eb65d3c1dc6
James Singleton <jsing25@users.noreply.github.com>
2016-01-27 21:15:28 -0600

    Update main.cpp

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -1,20 +1,31 @@
 
 /********************************************/
 /*    Groovy Music End-of-Year Inventory    */
-/*	          James Singleton           */
-/* 	       CSC 3380_97 Section 1        */
-/*                 Assignment 1             */
+/*	        James Singleton             */
+/*            CSC 3380_97 Section 1         */
+/*               Assignment 1               */
 /* Instructor: Douglas  	            */
 /* Program due date:  February 2, 2016      */
 /********************************************/
 
-// PROBLEM SPECIFICATION...
+/* PROBLEM SPECIFICATION: To create a functioning DVD inventory list that allows removal of outdated items.
 
-// PROBLEM ANALYSIS...
+   PROBLEM ANALYSIS: This program will take input from a data file containing the store DVD inventory.
+   It will request a cut off date from the user via command line then remove all items at or below the cut off date.
+   The program will display the original list, the items to be removed, and the updated list in a file named "Report.out".
+	
+   IMPLEMENTATION DIRECTIONS: The program consists of two cpp files named main.cpp and DVD_Data.cpp. 
+   To compile the program, navigate to the directory prog1 and use the g++ compiler in the terminal.
+   Execute the following commands via the terminal:
+    - To compile the program: g++ -c DVD_Data.cpp
+                              g++ -c main.cpp
+                              g++ DVD_Data.o main.o -o prog1
+    - To execute the program: ./prog1
+	   
+   The program takes input from an input file named "Music2015.data" and prints output to a file named "Report.out".
+*/
 
-// IMPLEMENTATION DIRECTIONS...
-
-#include "CD_Data.h";
+#include "DVD_Data.h"
 using namespace std;
 
 
@@ -24,16 +35,16 @@ int main()
 	inFile.open("Music2015.data");
 	ofstream outFile;
 	outFile.open("Report.out");
-	CD_Data *test = new CD_Data;
+	DVD_Data *inventory = new DVD_Data;
 
-	test->insert(inFile);
-	test->display(outFile);
-	test->get_total_inventory_value(outFile);
+	inventory->insert(inFile);
+	inventory->display(outFile);
+	inventory->get_total_inventory_value(outFile);
 
-	test->scan_for_removal(outFile, test->get_cut_off_date());
+	inventory->scan_for_removal(outFile, inventory->get_cut_off_date());
 
-	test->display(outFile);
-	test->get_total_inventory_value(outFile);
+	inventory->display(outFile);
+	inventory->get_total_inventory_value(outFile);
 
 	outFile.close();
 	return 0;
